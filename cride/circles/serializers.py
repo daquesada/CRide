d61121f343971bc1ab3415225b9fe9f2425373ab
daniel <daquesada@unicauca.edu.co>
2021-04-15 12:28:49 -0500

    add circles serializers

--- cride/circles/serializers.py
+++ cride/circles/serializers.py
@@ -1,7 +1,10 @@
 """Circle serializers"""
-
+#Django rest framework
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
+#models from cricle
+from .models import Circle
 class CircleSerializer(serializers.Serializers):
     """Circle serializers"""
     name = serializers.CharField()
@@ -9,3 +12,11 @@ class CircleSerializer(serializers.Serializers):
     rides_taken = serializers.IntegerField()
     rides_offered = serializers.IntegerField()
     members_limit = serializers.IntegerField()
+
+class CreateCircleSerializer(serializers.Serializer):
+    name = serializers.ChardField(max_length=140)
+    slug_name = serializers.SlugField(max_length=40,validators=[UniqueValidator(queryset=Circle.objects.all())])
+    about = serializers.ChardField(max_length=255, required=False)
+
+    def create(self,data):
+        return Circle.objects.create(**data)
